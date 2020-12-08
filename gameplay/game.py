6f2e53d51f27a9bfb414434e545e9389c332b501
Sibe Bleuzé <sibebleuze@gmail.com>
2020-12-08 22:15:06 +0100

    if player is on tempus, everything goes to next player (i think)

--- gameplay/game.py
+++ gameplay/game.py
@@ -57,6 +57,8 @@ class Game():
         player = self.players[names.index(player)]  # find the correct Player in the list of active players
         if (status, player.tempus) in [("ex", True), ("in", False)]:  # if a player isn't already in the desired state
             player.switch_tempus()  # switch the players tempus state
+            while self.beurt.tempus:
+                self.beurt == self.beurt.next_player
             if player.tempus:  # define a message addressing the player for the bot to send
                 response = f"{player.name} heeft nu tempus, tot zo!"
             else:
