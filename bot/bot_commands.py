86c18993a685594b1480667539594a5f196d89b9
Sibe Bleuzé <sibebleuze@gmail.com>
2021-02-21 22:02:37 +0100

    added spam filter for messages that are not game related (don't have '3man' prefix)

--- bot/bot_commands.py
+++ bot/bot_commands.py
@@ -368,6 +368,16 @@ class Comms(commands.Cog, name="DriemanBot commando's"):
             return
         await self.bot.process_commands(
             message)  # process all other commands first, if they got nothing, only then proceed
+        if message.author != self.bot.user and message.content != "vice kapot":
+            messages = await channel.history(limit=50).flatten()
+            mssgs = []
+            for mssg in messages:  # check if someone is spamming the channel with non game related messages
+                if mssg.author == message.author and mssg.content == message.content:
+                    mssgs.append(mssg)
+            if len(mssgs) > 3:  # is someone is spamming the channel, delete these spam messages
+                for mssg in mssgs:
+                    await mssg.delete()
+                await channel.send(f"{message.author.mention}, alleen ik mag dit kanaal volspammen.")
         if message.author == self.bot.user or message.content != "vice kapot":  # only respond to this message
             return
         with open('../bot/.secret', 'r') as file:  # get the access list for this command from the .secret file
